a2830be080d26a73916126fdb46690ae53c8297c
Meixii <iflrix2704@gmail.com>
2023-05-13 12:19:14 +0800

    Final v1
    Fixed Intro

--- html/js/script.js
+++ html/js/script.js
@@ -1,30 +1,3 @@
-// Mute button
-var audio = document.getElementById("audio");
-var muteButton = document.getElementById("mute-button");
-
-muteButton.addEventListener("click", function() {
-    if (audio.muted) {
-        audio.muted = false;
-        muteButton.src = "html/Assets/sound.png";
-    } else {
-        audio.muted = true;
-        muteButton.src = "html/Assets/no-sound.png";
-    }
-});
-
-// Fade out when switching webpage
-function fadeOutAudio() {
-let volume = 1;
-const interval = setInterval(() => {
-volume -= 0.1;
-audio.volume = volume;
-if (volume <= 0) {
-clearInterval(interval);
-audio.pause();
-}
-}, 100);
-}
-
 // Main Logo Redirect Delay
 const swapWebBtn = document.getElementById('swapWeb');
 swapWebBtn.addEventListener('click', () => {
